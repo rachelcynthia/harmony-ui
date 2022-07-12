869e68b9164384ffaa9ae24c8895b93288e7ce5e
Rachel Cynthia <rachelwork@Rachels-MacBook-Pro.local>
2022-07-12 20:41:53 +0530

    Update login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,28 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import Header from '../Header/Header';
 import './Login.css'
 import logo from '../../assets/logo.png'
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const handleSubmit = () => {
         // const user = `http://localhost:8080/blog/all`;
-        fetch('http://localhost:8080/blog/all')
-        .then(async response => {
-            const data = await response.json();
+        // fetch('http://localhost:8080/blog/all')
+        // .then(async response => {
+        //     const data = await response.json();
 
-            // check for error response
-            if (!response.ok) {
-                // get error message from body or default to response statusText
-                return Promise.reject(data);
-            }
+        //     // check for error response
+        //     if (!response.ok) {
+        //         // get error message from body or default to response statusText
+        //         return Promise.reject(data);
+        //     }
 
-           console.log(data)
-        })
-        .catch(error => {
-            console.error('There was an error!', error);
-        });
-        return true;
+        //    console.log(data)
+        // })
+        // .catch(error => {
+        //     console.error('There was an error!', error);
+        // });
+        // return true;
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email: email, password: password })
+        };
+        fetch('http://localhost:8080/login', requestOptions)
+            .then(async response => {
+                const isJson = response.headers.get('content-type')?.includes('application/json');
+                const data = isJson && await response.json();
+                console.log("registered", data)
+                if (response.ok) {
+                    window.location.href = "/home"
+                }
+            });
+
     }
     return (
         <>
@@ -34,9 +51,9 @@ const Login = () => {
                     </div>
                     <div className='login_body'>
                         <img src={logo} alt="Logo" width="80" height="80" />
-                        <input type="text" placeholder='Enter email' className="input_text" />
-                        <input type="password" placeholder='Enter password' className="input_text" />
-                        <input type="button" onClick={handleSubmit()} className="input_button" value="LOGIN" />
+                        <input type="text" placeholder='Enter email' className="input_text" onChange={(e) => setEmail(e.target.value)}/>
+                        <input type="password" placeholder='Enter password' className="input_text" onChange={(e) => setPassword(e.target.value)}/>
+                        <input type="button" onClick={handleSubmit} className="input_button" value="LOGIN" />
                     </div>
                     <div className='reg_component'>
                         New to Harmony? <Link to="/register" className='register_link'>Register</Link>
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Header from "../Header/Header";
 import './Register.css'
 import arrowLeft from "../../assets/arrow-left.svg"
 
 const Register = () => {
-    const handleSubmit = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
-    }
+    const handleSubmit = () => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ name: name, username: username, email: email, password: password, "confirm-password": confirmPassword })
+        };
+        fetch('http://localhost:8080/register', requestOptions)
+            .then(async response => {
+                const isJson = response.headers.get('content-type')?.includes('application/json');
+                const data = isJson && await response.json();
+                console.log("registered", data)
+                if (response.ok) {
+                    window.location.href = "/login"
+                }
+            });
+
+    };
+
     return (
         <>
             <Header isLoggedIn={false} />
@@ -17,16 +38,16 @@ const Register = () => {
                         REGISTER
                     </div>
                     <div className="register_body">
-                        <input type="text" placeholder='Enter name' className="input_text" />
-                        <input type="email" placeholder='Enter email' className="input_text" />
-                        <input type="email" placeholder='Enter username/display name' className="input_text" />
-                        <input type="password" placeholder='Enter password' className="input_text" />
-                        <input type="password" placeholder='Re enter your password' className="input_text" />
+                        <input type="text" placeholder='Enter name' className="input_text" onChange={(e) => setName(e.target.value)} />
+                        <input type="email" placeholder='Enter email' className="input_text" onChange={(e) => setEmail(e.target.value)} />
+                        <input type="text" placeholder='Enter username/display name' className="input_text" onChange={(e) => setUsername(e.target.value)} />
+                        <input type="password" placeholder='Enter password' className="input_text" onChange={(e) => setPassword(e.target.value)} />
+                        <input type="password" placeholder='Re enter your password' className="input_text" onChange={(e) => setConfirmPassword(e.target.value)} />
                         {/* <label for="profile">Upload your photo</label>
                         <input type="file" id="profile" accept="image/png, image/jpeg"></input> */}
-                        <input type="button" onClick={handleSubmit()} className="input_button" value="REGISTER" />
+                        <input type="button" onClick={handleSubmit} className="input_button" value="REGISTER" />
                         <div className="log_component">
-                        <img src={arrowLeft} alt="" height="25" width="25" color="red" className="login-left-arrow"></img>
+                            <img src={arrowLeft} alt="" height="25" width="25" color="red" className="login-left-arrow"></img>
                             <Link to="/login" className='login_link'>
                                 Back to Login
                             </Link>
